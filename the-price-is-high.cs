cc07c8f0e21bce59144a7c55be78c25b83fe2ba9
Vital Vuillaume <vitalvuillaume@proton.me>
2024-01-28 18:47:31 +0100

    Add files via upload

---
+++ the-price-is-high.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+            
+            // Random nb
+            Random rng = new Random();
+            int nbRng = rng.Next(0, 101);
+
+            // Input player
+            int inputResult;
+            bool isValidInput = false;
+            
+            // Sentence
+            string sentence = "Entrez un nombre entre 1 et 100 : ";
+            Console.WriteLine(sentence);
+            
+            do
+            {
+                string input = Console.ReadLine();
+                isValidInput = int.TryParse(input, out inputResult);
+                
+                if (!isValidInput)
+                {
+                    Console.WriteLine(sentence);
+                }
+                else if (inputResult > nbRng)
+                {
+                    Console.WriteLine("Le nombre " + inputResult + " est trop grand.");
+                }
+                else if (inputResult < nbRng)
+                {
+                    Console.WriteLine("Le nombre " + inputResult + " est trop petit.");
+                }
+                
+            } while (!isValidInput || inputResult != nbRng);
+
+            // Result
+            Console.WriteLine("Bien joue c'etait le nombre " + nbRng + ".");
+        }
+    }
+}
